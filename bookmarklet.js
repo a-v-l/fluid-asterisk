21bf4772e4508e38ebd1f062c091cd72e87b70cb
Arndt von Lucadou <a-v-l@gmx.de>
2013-05-08 01:02:22 +0300

    helper-function: paragraph width in characters
    helper-function zur Ermittlung der aktuellen Breite des ersten <p> in Zeichen (mit jQuery)

--- bookmarklet.js
+++ bookmarklet.js
@@ -9,3 +9,26 @@ jQuery('p').css("cursor","pointer").hover(function(){
   }
   jQuery(this).text(t);
 });
+
+/***
+ * helper-function zur Ermittlung der aktuellen Breite des ersten <p> in Zeichen
+ *
+
+(function($){
+    $(window).resize(function(){
+        var txt = $('p').first().text(),
+            p_width = $('p').first().width();
+            n = 0;
+        if($('#rr').length === 0){
+            $('body').append('<p id="rr"></p>');
+        }
+        $('#rr').text(txt[0]).css('width', p_width);
+        var rr_height = $('#rr').height();
+        while($('#rr').height() === rr_height) {
+            n++;
+            $('#rr').append(txt.substr(n,1));
+        }
+        console.log(n+1);
+    });
+}(jQuery))
+*/
