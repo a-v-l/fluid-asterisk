7e5b7f0a68d13000aeb15175d704054f33780903
Arndt von Lucadou <info@lucadou.net>
2013-06-15 11:23:08 +0200

    No asterisks anymore: background color & char counter
    - The range between 45-75th character is painted in red
    - On the choosen paragraph a indice with the current width of the element is shown

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,34 +1,63 @@
-jQuery('p').css("cursor","pointer").hover(function(){
-  jQuery(this).css("box-shadow", "0 0 0.5em rgba(0,0,0,0.5)");
-}, function(){
-  jQuery(this).css("box-shadow", "0 0 0 transparent");
-}).on('click', function(){
-  var t = jQuery(this).text();
-  if(t.length >= 75){
-    t = t.substr(0,44) + "*" + t.substr(45,29) + "*" + t.substring(75,t.length);
-  }
-  jQuery(this).text(t);
-});
-
-/***
- * helper-function zur Ermittlung der aktuellen Breite des ersten <p> in Zeichen
- *
-
 (function($){
-    $(window).resize(function(){
-        var txt = $('p').first().text(),
-            p_width = $('p').first().width();
-            n = 0;
-        if($('#rr').length === 0){
-            $('body').append('<p id="rr"></p>');
-        }
-        $('#rr').text(txt[0]).css('width', p_width);
-        var rr_height = $('#rr').height();
-        while($('#rr').height() === rr_height) {
-            n++;
-            $('#rr').append(txt.substr(n,1));
-        }
-        console.log(n+1);
+  $('head').append('<style type="text/css">rr{background-color:red}#range-checker{position:absolute;visibility:hidden}</style>');
+  $('body').append('<div id="range-indice"></div>');
+  
+ /***
+  * helper-function zur Ermittlung der aktuellen Breite des ersten <p> in Zeichen
+  */
+  var checkRR = function(elem){
+    var txt = elem.text(),
+      p_width = elem.width();
+      n = 0;
+    if($('#range-checker').length === 0){
+      $('body').append('<p id="range-checker"></p>');
+    }
+    rchecker = $('#range-checker');
+    rchecker.text(txt[0]).css('width', p_width);
+    var rr_height = rchecker.height();
+    while(rchecker.height() === rr_height) {
+      n++;
+      rchecker.append(txt.substr(n,1));
+    }
+    var off = elem.offset();
+    off.left += elem.outerWidth();
+    $('#range-indice').text(n+1).css({
+      position: "absolute",
+      top: off.top,
+      left: off.left
     });
-}(jQuery))
-*/
+  };
+  
+  var el;
+  $('p').css("cursor","pointer").hover(function(){
+    $(this).css("box-shadow", "0 0 0.5em rgba(0,0,0,0.5)");
+  }, function(){
+    $(this).css("box-shadow", "0 0 0 transparent");
+  }).on('click', function(){
+    el = $(this);
+    var t = el.text(),
+      l = el.find('rr').length,
+      start = '',
+      end = '',
+      between = '',
+      c = el.css('color');
+    if(t.length >= 75 && l == 0){
+      start = t.substr(0,44);
+      end = t.substring(75,t.length);
+      for ( i = 0; i <= 30; i++ ) {
+      	between += "<rr>" + t.substr(44 + i,1) + "</rr>"
+      }
+      //t = t.substr(0,44) + "*" + t.substr(45,29) + "*" + t.substring(75,t.length);
+      el.html(start + between + end);
+    } else if(l > 0){
+      $(this).html(t);
+    }
+    checkRR(el);
+  });
+  $(window).resize(function(){
+    if(el !== 'undefined'){
+      checkRR(el);
+    }
+  })
+}(jQuery));
+
